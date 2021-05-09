82925c9cc1762c9230b2e53da7bd733fba0bac52
FeketeMiklosBenjamin <fekete.miklos.benjamin@students.jedlik.eu>
2021-05-09 18:40:57 +0200

    A hasáb felszíne

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ def main() -> None:
     b : float = float(input('b (él) = '))
     c : float = float(input('c (él) = '))
     if a < 1 or b < 1 or c < 1:
-        print('A megadott adattal nem lehet számolni!')
+        print('A megadott adatokkal nem lehet számolni!')
     else:
         hasáb_terfogata: float = a * b * c
         print(f'A hasáb térfogata: {hasáb_terfogata}')
+
+    print('A hasáb felszínét kiszámoló függvény!')
+    a : float = float(input('a (él) = '))
+    b : float = float(input('b (él) = '))
+    c : float = float(input('c (él) = '))
+    if a < 1 or b < 1 or c < 1: 
+        print('A megadott adatokkal nem lehet számolni!')
+    else:
+        hasáb_felszine: float = (2 * a * b) + (2 * a * c) + (2 * b * c)
+        print(f'A hasáb felszíne: {hasáb_felszine}')
 
 if __name__ == "__main__":
     main()
